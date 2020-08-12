8433805d716202d8ef7694104a20890757d6882b
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-12 18:11:51 -0400

    Added a portfolio header, so that the design of the pages could keep consistent. Changed the file paths for the About Me page images, but the images are still not rendering on the screen. Will need to look into the file paths again to correct the error.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import Header from "./components/Header/Header";
-// import About from "./components/About/About";
-// import Portfolio from "./components/Portfolio/Portfolio";
+// import About from "./components/About";
 // import Contact from "./components/Contact/Contact";
 // import Footer from './components/Footer/Footer';
 import Portfolio from "./components/Portfolio";
 import "./App.css"
+import PortfolioHeader from "./components/PortfolioHeader";
 // import Project from "./components/Project/Project";
 
 function App() {
   return (
+    <>
+    <PortfolioHeader />
       <Portfolio />
+      </>
   );
 }
 
---
+++ src/components/PortfolioHeader/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PortfolioHeader() {
+    return (
+        <header className="card-header">
+            <h3>Portfolio</h3>
+        </header>
+    );
+}
+
+export default PortfolioHeader;

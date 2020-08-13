1fa55ba3e777bc32c3af62e077f61a6acd2d038e
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-12 21:49:24 -0400

    Fixed the sticky footer. Added padding to the myApp container, so that the page is no longer cut off. Resolved the issue with the JSON object as it seemed that I was naming the attribute of the project title incorrectly. Need to resolve issues with the portfolio page of the application.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -14,49 +14,49 @@ function Portfolio() {
         repository={project[0].repository}
     />
     <Project
-        project={project[1].title}
+        title={project[1].title}
         image={project[1].image}
         deployed={project[1].deployed}
         repository={project[1].repository}
     />
     <Project
-        project={project[2].title}
+        title={project[2].title}
         image={project[2].image}
         deployed={project[2].deployed}
         repository={project[2].repository}
     />
     <Project
-        project={project[3].title}
+        title={project[3].title}
         image={project[3].image}
         deployed={project[3].deployed}
         repository={project[3].repository}
     />
     <Project
-        project={project[4].title}
+        title={project[4].title}
         image={project[4].image}
         deployed={project[4].deployed}
         repository={project[4].repository}
     />
     <Project
-        project={project[5].title}
+        title={project[5].title}
         image={project[5].image}
         deployed={project[5].deployed}
         repository={project[5].repository}
     />
     <Project
-        project={project[6].title}
+        title={project[6].title}
         image={project[6].image}
         deployed={project[6].deployed}
         repository={project[6].repository}
     />
     <Project
-        project={project[7].title}
+        title={project[7].title}
         image={project[7].image}
         deployed={project[7].deployed}
         repository={project[7].repository}
     />
     <Project
-        project={project[8].title}
+        title={project[8].title}
         image={project[8].image}
         deployed={project[8].deployed}
         repository={project[8].repository}

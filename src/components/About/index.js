6e84a11bae2e2884bdf7fdb9186e03e9cd3edae7
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-24 21:40:16 -0400

    Made changes to the About page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,12 +13,12 @@ function About() {
           </section>
           <p><b>
             <br></br>
-            After having completed my undergraduate degree studies at New York University, I moved back to Philadelphia to work in social services as a youth counselor in the hopes of putting my degrees to use. After more than six years of social work (and so many heartfelt, inspiring, and traumatic anecdotes to fill the walls of the Bodleian Library!), I'm now working towards a career change as a full stack developer.</b></p>
+            Having completed my undergraduate degree studies at New York University, I moved back to Philadelphia to work in social services as a youth counselor in the hopes of putting my degrees to use. After six years of social work (and so many heartfelt, inspiring, and traumatic anecdotes to fill the walls of the Bodleian Library!), I'm now seeking a position as a full stack web developer.</b></p>
           <p><b>I'm looking forward to coding for you!</b></p>
           <br></br>
           <div className="socialIcons" id="socialIcons">
           <a href="https://github.com/rh9891" target="_blank"><img src="assets/githubLogo.png" alt="Github Logo"/></a>
-          <a href="https://docs.google.com/document/d/1Xg6i4LC_tL8g8xhu4_Ug1dFWxH5wnr68QzEwWe-yeq0/edit?usp=sharing " target="_blank"><img src="assets/magnifyingGlassLogo.png" alt="Magnifying Glass Logo" id="magnifyingLogo"/></a>
+          <a href="https://docs.google.com/document/d/1sDxK6bB5T72BncLHZVYwIBZ-NCHIX9oRrvebN9T21bc/edit?usp=sharing" target="_blank"><img src="assets/magnifyingGlassLogo.png" alt="Magnifying Glass Logo" id="magnifyingLogo"/></a>
           <a className="float-left linkedIn" href="https://www.linkedin.com/in/romie-hecdivert-a75a181a8/" target="_blank"><img src="assets/linkedInLogo.png" alt="LinkedIn Logo"/></a>
         </div>
         </section>
